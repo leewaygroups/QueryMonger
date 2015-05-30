5231657894504f65d37fd7c467c23f0036cdec5b
Prince <leewaygroups@gmail.com>
2015-05-30 10:28:24 +0600

    TypeScript adopted for front-end

--- QueryMongerClient/App/App.ts
+++ QueryMongerClient/App/App.ts
@@ -1,47 +1,50 @@
-var queryMonger;
-(function (queryMonger) {
-    var Query = (function () {
-        function Query() {
-        }
-        Query.validateSearchTerm = function () {
+﻿module queryMonger {
+    export class Query {
+
+        static validateSearchTerm() {
             var regExp = /\w+/;
             var val = document.getElementById("#searchMain").nodeValue;
             if (regExp.test(val)) {
                 alert(val);
             }
-        };
-        Query.prototype.renderAllQuery = function () {
+        }
+
+        renderAllQuery() {
             $.ajax({
                 method: "GET",
                 url: "http://localhost:52499/api/queries",
                 success: function (response) {
-                    var query = { query: response };
+                    var query = { query: response }
                     this.renderData(query);
                 }
             });
-        };
-        Query.prototype.renderData = function (data) {
+        }
+
+        renderData(data) {
             var template = this.renderTemplate();
             $("#lisItemtHead").html(template(data));
-        };
-        Query.prototype.renderTemplate = function () {
+        }
+
+        renderTemplate() {
             var listItemTemplate = Handlebars.compile($("#listItemTemplate").html());
+
             return listItemTemplate;
-        };
-        return Query;
-    })();
-    queryMonger.Query = Query;
-})(queryMonger || (queryMonger = {}));
+        }
+
+    }
+}
+
 $(document).ready(function () {
     var queryInstance = new queryMonger.Query();
     queryInstance.renderAllQuery();
+
     var querySearch = document.getElementById("#btnsearchMain");
     querySearch.addEventListener("click", function () {
         queryMonger.Query.validateSearchTerm();
     });
+
     var searchForm = document.getElementById("#frmSearchMain");
     searchForm.addEventListener("submit", function () {
         queryMonger.Query.validateSearchTerm();
     });
 });
-//# sourceMappingURL=App.js.map
